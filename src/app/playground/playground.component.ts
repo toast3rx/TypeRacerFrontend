9c7c612f210af14b2cc9fcdaddd4905566bbafdb
Mihai Latea <latea.alexandru@yahoo.com>
2022-10-21 00:10:34 +0300

    Changed typescript strict property intialization configuration to false. Added buttons for starting the game, resetting the timer and fields for counting typing speed and words typed. Input field now disabes before the game starts and after

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -6,14 +6,38 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./playground.component.css'],
 })
 export class PlaygroundComponent implements OnInit {
-  typedValue: string = '';
+  @ViewChild('typingField') typingField: ElementRef;
 
+  // Text showing in input field
+  typedValue: string;
+
+  // String coming from api
   words: string = 'hello world foo bar';
-  wordsArray = this.words.split('');
-  correctWords: string[] = [];
-  wrongWords: string[] = [];
 
-  constructor() {}
+  wordsArray: string[];
+  correctWords: string[];
+  wrongWords: string[];
+
+  timeLeftInSeconds: number;
+  private totalTimeInSeconds: number;
+  interval: NodeJS.Timer;
+
+  wordsTyped: number;
+  typingSpeed: number;
+
+  constructor() {
+    this.wordsArray = this.words.split('');
+    this.correctWords = [];
+    this.wrongWords = [];
+
+    this.typedValue = '';
+
+    this.timeLeftInSeconds = 60;
+    this.totalTimeInSeconds = 60;
+
+    this.wordsTyped = 0;
+    this.typingSpeed = 0;
+  }
 
   ngOnInit(): void {}
 
@@ -26,6 +50,7 @@ export class PlaygroundComponent implements OnInit {
       if (this.wordsArray[0] === value?.data && this.wrongWords.length === 0) {
         if (value.data === ' ') {
           this.typedValue = '';
+          this.wordsTyped++;
         }
         this.correctWords.push(value.data);
         this.wordsArray.shift();
@@ -37,5 +62,58 @@ export class PlaygroundComponent implements OnInit {
       let currentLetter = this.wrongWords.pop() as string;
       this.wordsArray.unshift(currentLetter);
     }
+
+    if (this.correctWords.length === this.words.length) {
+      this.wordsTyped++;
+      this.stopGame();
+    }
+  }
+
+  /**
+   * Starts the game by enabling typing and starting the timer
+   */
+  startGame() {
+    this.typingField.nativeElement.disabled = false;
+    this.typingField.nativeElement.focus();
+    this.startTimer();
+  }
+
+  /**
+   * Stops the game by disabled typing and calculating typing speed
+   */
+  stopGame() {
+    this.typingField.nativeElement.disabled = true;
+    this.setTypingSpeed();
+  }
+
+  //////// Timer Methods ////////
+  startTimer() {
+    this.typingField.nativeElement.disabled = false;
+    this.typingField.nativeElement.focus();
+    this.interval = setInterval(() => {
+      if (this.timeLeftInSeconds > 0) {
+        this.timeLeftInSeconds--;
+      }
+    }, 1000);
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.timeLeftInSeconds = 60;
+  }
+
+  stopTimer() {
+    clearInterval(this.interval);
+  }
+
+  /**
+   * Calculate typing speed using time left on timer, total words typed
+   */
+  setTypingSpeed() {
+    clearInterval(this.interval);
+    this.typingSpeed = Math.trunc(
+      (60 * this.wordsTyped) /
+        (this.totalTimeInSeconds - this.timeLeftInSeconds)
+    );
   }
 }
